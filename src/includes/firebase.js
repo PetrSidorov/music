e9de6ceeb2099e7b40dca844867e00f52f5084bb
Peter <piterpatrikk@gmail.com>
2021-08-09 22:46:59 +0300

    Main page, routing, registration form, login form, uploading mp3's, firebase connection

---
+++ src/includes/firebase.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBAa-T1hJoUisaNeS7HX5KP41mMwClqEGY',
+  authDomain: 'music-85f7d.firebaseapp.com',
+  projectId: 'music-85f7d',
+  storageBucket: 'music-85f7d.appspot.com',
+  appId: '1:930092045201:web:206f6452a556296c85c030',
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+const usersCollection = db.collection('users');
+
+export {
+  auth,
+  db,
+  usersCollection,
+  storage,
+};
---
+++ src/router/index.js
@@ -0,0 +1,58 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
+import Manage from '@/views/Manage.vue';
+import store from '@/store';
+
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: Home,
+  },
+  {
+    name: 'about',
+    path: '/about',
+    component: About,
+  },
+  {
+    name: 'manage',
+    meta: {
+      requiresAuth: true,
+    },
+    // alias: '/manage-music',
+    path: '/manage-music',
+    component: Manage,
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' },
+    beforeEnter: (next) => {
+      console.log('Manage guard');
+      next();
+    },
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' },
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+  linkExactActiveClass: 'text-yellow-500',
+});
+
+router.beforeEach((to, from, next) => {
+  if (!to.matched.some((record) => record.meta.requiresAuth)) {
+    next();
+    return;
+  }
+  if (store.state.userLoggedIn) {
+    next();
+  } else {
+    next({ name: 'home' });
+  }
+});
+export default router;
