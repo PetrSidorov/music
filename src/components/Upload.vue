99b79b044aa81355a37ac0c5f947b192e130768b
Peter <piterpatrikk@gmail.com>
2021-08-10 00:17:23 +0300

    before unount for dealing situation when user leaves page while uploading + usual upload for files

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -22,6 +22,7 @@
                 >
               <h5>Drop your files here</h5>
             </div>
+            <input multiple type="file" @change="upload($event)"/>
             <hr class="my-6" />
             <!-- Progess Bars -->
             <div class="mb-4" v-for="upload in uploads" :key="upload.name">
@@ -40,7 +41,7 @@
       </div>
 </template>
 <script>
-import { storage } from '@/includes/firebase';
+import { storage, auth, songsCollection } from '@/includes/firebase';
 
 export default {
   name: 'Upload',
@@ -53,7 +54,9 @@ export default {
   methods: {
     upload($event) {
       this.is_dragover = false;
-      const files = [...$event.dataTransfer.files];
+      const files = $event.dataTransfer
+        ? [...$event.dataTransfer.files]
+        : [...$event.target.files];
       files.forEach((file) => {
         if (file.type !== 'audio/mpeg') {
           return;
@@ -78,13 +81,35 @@ export default {
           this.uploads[uploadIndex].icon = 'fas fa-times';
           this.uploads[uploadIndex].text_class = 'text-red-400';
           console.log(error);
-        }, () => {
+        }, async () => {
+          const song = {
+            uid: auth.currentUser.uid,
+            display_name: auth.currentUser.displayName,
+            original_name: task.snapshot.ref.name,
+            modified_name: task.snapshot.ref.name,
+            genre: '',
+            comment_count: 0,
+          };
+
+          song.url = await task.snapshot.ref.getDownloadURL();
+          await songsCollection.add(song);
+
           this.uploads[uploadIndex].variant = 'bg-green-400';
           this.uploads[uploadIndex].icon = 'fas fa-check';
           this.uploads[uploadIndex].text_class = 'text-green-400';
         });
       });
     },
+    cancelUploads() {
+      this.uploads.forEach((upload) => {
+        upload.task.cancel();
+      });
+    },
+  },
+  beforeUnmount() {
+    this.uploads.forEach((upload) => {
+      upload.task.cancel();
+    });
   },
 };
 </script>
